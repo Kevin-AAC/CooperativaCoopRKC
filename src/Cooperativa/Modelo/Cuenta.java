4fb511414213432e6de2d381875a58f0c88e3eae
Kevin-AAC <kevin.argumedo@est.iudigital.edu.co>
2025-09-20 22:20:30 -0500

    Feat:logica clase Cuenta

--- src/Cooperativa/Modelo/Cuenta.java
+++ src/Cooperativa/Modelo/Cuenta.java
@@ -1,4 +1,28 @@
 package Cooperativa.Modelo;
 
 public class Cuenta {
+    private String numeroCuenta;
+    private double saldo;
+    private String tipo;
+
+
+    public Cuenta(String numeroCuenta, double saldo, String tipo) {
+        this.numeroCuenta = numeroCuenta;
+        this.saldo = saldo;
+        this.tipo = tipo;
+    }
+
+    public void deposito(double monto){
+        saldo += monto;
+
+    }
+
+    public void retirar(double monto){
+        if(saldo < monto){
+            System.out.println("Saldo Insuficiente");
+        }else {
+            System.out.println("Retiro Exitoso");
+            saldo -= monto;
+        }
+    }
 }
