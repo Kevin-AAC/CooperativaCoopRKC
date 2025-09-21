c2348ba136c5d418c00ae567db6737691fcb2156
Kevin-AAC <kevin.argumedo@est.iudigital.edu.co>
2025-09-20 23:15:51 -0500

    Feat:Logica clase Cooperativa

--- src/Cooperativa/Modelo/Cooperativa.java
+++ src/Cooperativa/Modelo/Cooperativa.java
@@ -1,4 +1,26 @@
 package Cooperativa.Modelo;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Cooperativa {
+    private String nombre;
+    private String direccion;
+    private String codigo;
+
+    private List<Socio> socios = new ArrayList<>();
+
+    public Cooperativa(String nombre, String direccion, String codigo) {
+        this.nombre = nombre;
+        this.direccion = direccion;
+        this.codigo = codigo;
+    }
+
+    public void agregarSocio(Socio socio){
+        socios.add(socio);
+    }
+
+    public List<Socio> getSocios(){
+        return socios;
+    }
 }
