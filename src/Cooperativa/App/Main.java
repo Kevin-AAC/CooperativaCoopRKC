6c16c8f2b77a950ed208a27bacc1c33258c4257a
Kevin-AAC <kevin.argumedo@est.iudigital.edu.co>
2025-10-02 23:19:09 -0500

    feat:metodo listar socio, sumaTotalcooperativa

--- src/Cooperativa/App/Main.java
+++ src/Cooperativa/App/Main.java
@@ -45,58 +45,16 @@ public class Main {
                 case 6:
                     filtarCuentas();
                     break;
+                case 7:
+                    listaSocios();
+                    break;
                 case 8:
-                    listaCuentas();
+                    sumaTotalCooperativa();
                     break;
 
 
             }
         }
-
-
-
-        /*
-        Socio socio1 = new Socio("Carpintero", "9876543");
-        Socio socio2 = new Socio("laura", "9876543");
-        Socio socio3 = new Socio("Carlos", "57532545");
-        Socio socio4 = new Socio("Jesus", "00120200");
-        */
-        CuentaAhorros cuenta = new CuentaAhorros("001", 1000.0, "Ahorros",0.02);
-        CuentaAhorros cuenta1 = new CuentaAhorros("002", 1000000, "Ahorros",0.01);
-        CuentaAhorros cuenta2 = new CuentaAhorros("123", 2300.0, "Ahorros",0.02);
-        CuentaAhorros cuenta3 = new CuentaAhorros("004", 750000.0, "Ahorros",0.05);
-        CuentaAhorros cuenta4 = new CuentaAhorros("12345678", 1000.0, "Ahorros",0.01);
-
-        System.out.println("==========Retirar============");
-        Retiro retirar = new Retiro (5000, cuenta1,"24/09/2025");
-        retirar.ejecutar();
-        Retiro retirar3 = new Retiro (50000, cuenta3,"24/09/2025");
-        retirar3.ejecutar();
-
-        System.out.println("==========Depositar============");
-        Deposito deposito = new Deposito(350000,cuenta1,"23/09/2025");
-        deposito.ejecutar();
-
-        Deposito deposito2 = new Deposito(150000,cuenta2,"23/09/2025");
-        deposito2.ejecutar();
-        System.out.println(deposito+"\n"+deposito2);
-
-        System.out.println("=============Interes==============");
-        cuenta2.intereses();
-        System.out.println("💳 Cuenta " + cuenta2.getNumeroCuenta() +
-                " | 📈 Interés E.A: " + (cuenta2.getInteres() * 100) + "%" +
-                " | 💰 Saldo: $" + String.format("%,.2f", cuenta2.getSaldo()));
-
-
-        cooperativa.getSocios().stream().map(Socio::getNombre).forEach(System.out::println);
-        System.out.println("===========Filtro de Cuentas ===========");
-        cooperativa.getCuentas().stream().filter(cuentas -> cuentas.getSaldo()>500000).map(cuentas -> String.format( "💳 Cuenta Nº %s | Saldo: $%,.2f",cuentas.getNumeroCuenta(),cuentas.getSaldo())).forEach(System.out::println);
-        System.out.println("===========Saldo Total Cooperativa ===========");
-        double saldoCooperativa =cooperativa.getSocios().stream().flatMap(socio -> socio.getListaCuenta().stream()).map(Cuenta::getSaldo).reduce(0.0,(a,b)->a+b);
-        System.out.println("\n💹 Suma total de saldos: $" + String.format("%,.2f", saldoCooperativa));
-        System.out.println("======================");
-        //System.out.println("Cooperativa info: " + cooperativa);
-
 
     }
     static void mostrarMenu(){
@@ -107,7 +65,8 @@ public class Main {
         System.out.println("4. Realizar Deposito");
         System.out.println("5. Asignar Cuenta");
         System.out.println("6. Filtar Cuenta");
-        System.out.println("8. Lista de Cuentas");
+        System.out.println("7. Listar socios");
+        System.out.println("8. sumaTotalCooperativa");
         System.out.println("9. Salir");
         System.out.print("Selecciona una opción: ");
     }
@@ -190,6 +149,14 @@ public class Main {
                 String.format("Cuenta %s | Saldo: $%,.2f", cuenta.getNumeroCuenta(), cuenta.getSaldo())
         ));
 
+    }
+    static void listaSocios(){
+        cooperativa.getSocios().stream().map(Socio::getNombre).forEach(System.out::println);
+    }
+    static void  sumaTotalCooperativa(){
+        System.out.println("===========Saldo Total Cooperativa ===========");
+        double saldoCooperativa =cooperativa.getCuentas().stream().mapToDouble(Cuenta::getSaldo).sum();
+        System.out.println("\n💹 Suma total de saldos: $" + String.format("%,.2f", saldoCooperativa));
     }
 
 
